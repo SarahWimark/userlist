28ed0a4b0d2c0dc468bca80ebdb15fcec35b7ff9
Sarah Wimark <sarahwimark@gmail.com>
2019-12-05 17:29:00 +0100

    Adding modal to display user info

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -26,30 +27,35 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <title>Userlist</title>
 </head>
+
 <body>
-<div class="container">  
-                <h3>Userlist</h3>  
-                <br />  
-                <div class="table-responsive">  
-                     <table id="userInfo" class="table table-striped table-bordered">  
-                          <thead>  
-                               <tr>  
-                                    <td>Id</td> 
-                                    <td>Name</td>  
-                                    <td>Email</td>  
-                                    <td>Created at</td>  
-                               </tr>  
-                          </thead>  
-                          <?php  
+    <div class="container">
+        <h3>Userlist</h3>
+        <br />
+        <div class="table-responsive">
+            <table id="userInfo" class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td>Id</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Created at</td>
+                    </tr>
+                </thead>
+                <?php  
+                          // Crate a table row for each user in the database
                           foreach($users as $user)  
                           {  
                                echo '  
                                <tr>  
+                                    <td><button class="infoBtn btn btn-primary" data-target="#myModal">User Details</button></td>  
                                     <td>'.$user["id"].'</td>  
                                     <td>'.$user["name"].'</td>  
                                     <td>'.$user["email"].'</td>  
@@ -57,14 +63,49 @@
                                </tr>  
                                ';  
                           }  
-                          ?>  
-                     </table>  
-                </div>  
-           </div>   
-           <script>  
-                $(document).ready(function(){  
-                    $('#userInfo').DataTable();  
-                });  
-            </script>  
+                          ?>
+            </table>
+        </div>
+
+         <!-- Modal -->
+        <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h3 id="modalTitle">User Info</h3>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    </div>
+                    <div class="modal-body"><p></p></div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+         <!-- End Modal -->
+
+    </div>
+
+    <script>
+        // Initialize the datatable
+        $(document).ready(function () {
+            const dataTable = $('#userInfo').DataTable();
+            
+            // Get userinfo from clicked row
+            $('#userInfo').on('click', 'button', function () {
+                const clickedRow = dataTable.row($(this).closest('tr'));
+                const id
+                const modalBody = `
+                     User id: ${clickedRow.data()[1]}
+                     Name: ${clickedRow.data()[2]} 
+                     Email: ${clickedRow.data()[3]}
+                     Created at: ${clickedRow.data()[4]}
+                     `;
+                $('#myModal .modal-body').html(modalBody);
+                $('#myModal').modal('toggle');
+            });
+        });
+    </script>
 </body>
+
 </html>
